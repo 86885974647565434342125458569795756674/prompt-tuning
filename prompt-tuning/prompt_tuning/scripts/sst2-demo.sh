dc89304707c89887caf9728e80f6be7d5ab6cca2
prompt-tuning <2903319587@qq.com>
2023-05-16 01:35:21 -0700

    ready to debug

--- prompt-tuning/prompt_tuning/scripts/sst2-demo.sh
+++ prompt-tuning/prompt_tuning/scripts/sst2-demo.sh
@@ -24,17 +24,32 @@ echo "============================="
 PRETRAINED_MODEL="gs://t5-data/pretrained_models/t5x/t5_1_1_lm100k_small/checkpoint_1100000"
 
 #
-python3 -m t5x.train \
-  --gin_search_paths="${T5X_DIR},${FLAXFORMER_DIR},${PROMPT_DIR}" \
-  --gin_file="prompt_tuning/configs/models/t5_1_1_small_prompt.gin" \
-  --gin_file="prompt_tuning/configs/prompts/from_class_labels.gin" \
-  --gin_file="prompt_tuning/configs/runs/prompt_finetune.gin" \
-  --gin.CLASS_LABELS="['positive', 'negative']" \
-  --gin.MODEL_DIR="'${MODEL_DIR}'" \
-  --gin.MIXTURE_OR_TASK_NAME="'taskless_glue_sst2_v200_examples'" \
-  --gin.MIXTURE_OR_TASK_MODULE="'prompt_tuning.data.glue'" \
-  --gin.TASK_FEATURE_LENGTHS="{'inputs': 512, 'targets': 8}" \
-  --gin.INITIAL_CHECKPOINT_PATH="'${PRETRAINED_MODEL}'" \
-  --gin.TRAIN_STEPS="1_150_000" \
-  --gin.USE_CACHED_TASKS="False" \
-  --tfds_data_dir=${TFDS_DATA_DIR}
+#python3 -m t5x.train \
+#  --gin_search_paths="${T5X_DIR},${FLAXFORMER_DIR},${PROMPT_DIR}" \
+#  --gin_file="prompt_tuning/configs/models/t5_1_1_small_prompt.gin" \
+#  --gin_file="prompt_tuning/configs/prompts/from_class_labels.gin" \
+#  --gin_file="prompt_tuning/configs/runs/prompt_finetune.gin" \
+#  --gin.CLASS_LABELS="['positive', 'negative']" \
+#  --gin.MODEL_DIR="'${MODEL_DIR}'" \
+#  --gin.MIXTURE_OR_TASK_NAME="'taskless_glue_sst2_v200_examples'" \
+#  --gin.MIXTURE_OR_TASK_MODULE="'prompt_tuning.data.glue'" \
+#  --gin.TASK_FEATURE_LENGTHS="{'inputs': 512, 'targets': 8}" \
+#  --gin.INITIAL_CHECKPOINT_PATH="'${PRETRAINED_MODEL}'" \
+#  --gin.TRAIN_STEPS="1_150_000" \
+#  --gin.USE_CACHED_TASKS="False" \
+#  --tfds_data_dir=${TFDS_DATA_DIR}
+
+echo  "python3 -m t5x.train"
+echo  --gin_search_paths="${T5X_DIR},${FLAXFORMER_DIR},${PROMPT_DIR}"
+echo  --gin_file="prompt_tuning/configs/models/t5_1_1_small_prompt.gin"
+echo  --gin_file="prompt_tuning/configs/prompts/from_class_labels.gin"
+echo  --gin_file="prompt_tuning/configs/runs/prompt_finetune.gin"
+echo  --gin.CLASS_LABELS="['positive', 'negative']"
+echo  --gin.MODEL_DIR="'${MODEL_DIR}'"
+echo  --gin.MIXTURE_OR_TASK_NAME="'taskless_glue_sst2_v200_examples'"
+echo  --gin.MIXTURE_OR_TASK_MODULE="'prompt_tuning.data.glue'"
+echo  --gin.TASK_FEATURE_LENGTHS="{'inputs': 512, 'targets': 8}"
+echo  --gin.INITIAL_CHECKPOINT_PATH="'${PRETRAINED_MODEL}'"
+echo  --gin.TRAIN_STEPS="1_150_000"
+echo  --gin.USE_CACHED_TASKS="False"
+echo  --tfds_data_dir=${TFDS_DATA_DIR}
